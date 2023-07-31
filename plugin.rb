11e0ff2fd43dc2bbe0f106559df69052db52a00a
Martin Brennan <martin@discourse.org>
2023-08-01 09:48:10 +1000

    Remove unread_private_messages usage
    c.f. https://github.com/discourse/discourse/pull/22893

--- plugin.rb
+++ plugin.rb
@@ -11,22 +11,32 @@ enabled_site_setting :enable_amazon_sns_pns
 after_initialize do
   require File.expand_path("../app/models/amazon_sns_subscription.rb", __FILE__)
   require File.expand_path("../lib/amazon_sns_helper.rb", __FILE__)
-  require File.expand_path('../jobs/regular/amazon_sns_notification.rb', __FILE__)
-  require File.expand_path("../app/controllers/amazon_sns_controller.rb", __FILE__)
+  require File.expand_path(
+            "../jobs/regular/amazon_sns_notification.rb",
+            __FILE__
+          )
+  require File.expand_path(
+            "../app/controllers/amazon_sns_controller.rb",
+            __FILE__
+          )
 
   Discourse::Application.routes.append do
-    post '/amazon-sns/subscribe' => "amazon_sns_subscription#create"
-    post '/amazon-sns/disable' => "amazon_sns_subscription#disable"
+    post "/amazon-sns/subscribe" => "amazon_sns_subscription#create"
+    post "/amazon-sns/disable" => "amazon_sns_subscription#disable"
   end
 
-  User.class_eval do
-    has_many :amazon_sns_subscriptions, dependent: :delete_all
-  end
+  User.class_eval { has_many :amazon_sns_subscriptions, dependent: :delete_all }
 
   DiscourseEvent.on(:post_notification_alert) do |user, payload|
     if user.amazon_sns_subscriptions.exists?
-      unread_total = user.unread_notifications + user.unread_private_messages
-      Jobs.enqueue(:amazon_sns_notification, user_id: user.id, payload: payload, unread: unread_total)
+      unread_total =
+        user.unread_notifications + user.unread_high_priority_notifications
+      Jobs.enqueue(
+        :amazon_sns_notification,
+        user_id: user.id,
+        payload: payload,
+        unread: unread_total
+      )
     end
   end
 end
