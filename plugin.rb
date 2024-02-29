459b75390b826a0937fa22076e50172b1040d100
Mark VanLandingham <markvanlan@gmail.com>
2024-02-29 08:38:09 -0600

    DEV: Add plugin modifier for filtering PNs

--- plugin.rb
+++ plugin.rb
@@ -23,6 +23,17 @@ after_initialize do
 
   DiscourseEvent.on(:push_notification) do |user, payload|
     if user.amazon_sns_subscriptions.exists?
+      send_notification = true
+      send_notification =
+        DiscoursePluginRegistry.apply_modifier(
+          :amazon_sns_send_notification,
+          send_notification,
+          user,
+          payload,
+        )
+
+      next if !send_notification
+
       unread_total = user.unread_notifications + user.unread_high_priority_notifications
       Jobs.enqueue(
         :amazon_sns_notification,
